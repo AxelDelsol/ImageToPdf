f2224a446b07c6dab2270dcf57f0d3ad9df63e8f
Axel Delsol <axel.delsol@laposte.net>
2020-09-10 10:11:30 +0200

    Added OutputDirectory class and tests

---
+++ ImageToPdf/ImageToPdf.Core/Services/OutputDirectory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ImageToPdf.Core.Services
+{
+    public class OutputDirectory
+    {
+        public string DirName { get; }
+        public OutputDirectory(string dirname)
+        {
+            if (Directory.Exists(dirname) == false)
+                throw new DirectoryNotFoundException("Invalid filename");
+
+            DirName = dirname;
+        }
+    }
+}
