19f665295533f2f00476a256457a936ea85587a7
Axel Delsol <axel.delsol@laposte.net>
2020-09-10 13:56:41 +0200

    Added MVVM Cross boilerplate

---
+++ ImageToPdf/ImageToPdf.Core/App.cs
@@ -0,0 +1,17 @@
+﻿using ImageToPdf.Core.ViewModels;
+using MvvmCross.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageToPdf.Core
+{
+    public class App : MvxApplication
+    {
+        public override void Initialize()
+        {
+            //Mvx.IoCProvider.RegisterType<ICalculationService, CalculationService>();
+            RegisterAppStart<ImageToPdfViewModel>();
+        }
+    }
+}
---
+++ ImageToPdf/ImageToPdf.Core/ViewModels/ImageToPdfViewModel.cs
@@ -0,0 +1,40 @@
+﻿using MvvmCross.Commands;
+using MvvmCross.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace ImageToPdf.Core.ViewModels
+{
+    public class ImageToPdfViewModel : MvxViewModel
+    {
+        private ObservableCollection<ImagePath> _queue;
+        public ObservableCollection<ImagePath> Queue
+        {
+            get => _queue;
+            set {
+                SetProperty(ref _queue, value);
+            }
+        }
+
+        private ObservableCollection<ImagePath> _done;
+        public ObservableCollection<ImagePath> Done
+        {
+            get => _done;
+            set
+            {
+                SetProperty(ref _done, value);
+            }
+        }
+
+
+        private void ResetText()
+        {
+            
+        }
+        public IMvxCommand ResetTextCommand => new MvxCommand(ResetText);
+        
+
+    }
+}
