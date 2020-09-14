eda9903a20b7363aa6596db6832c72873cb1e0c2
Axel Delsol <axel.delsol@laposte.net>
2020-09-14 12:46:44 +0200

    Renamed parameters and documentation in IConverted.cs

--- ImageToPdf/ImageToPdf.Core/Services/IConverter.cs
+++ ImageToPdf/ImageToPdf.Core/Services/IConverter.cs
@@ -7,19 +7,19 @@ namespace ImageToPdf.Core.Services
     public interface IConverter
     {
         /// <summary>
-        /// For each img in images, create a pdf file in outDir.
-        /// If img.FileName = "test.png", thea associated pdf file will be outDir\test.pdf
+        /// For each img in images, create a pdf file in outputDirectory.
+        /// If img.FileName = "test.png", the associated pdf file will be outDir\test.pdf.
         /// </summary>
-        /// <param name="images">Collections of images to convertt</param>
-        /// <param name="outDir"> Output directory</param>
-        void Convert(IEnumerable<ImagePath> images, OutputDirectory outDir);
+        /// <param name="images">Collections of images to convert.</param>
+        /// <param name="outputDirectory">Directory containing the converted images.</param>
+        void Convert(IEnumerable<ImagePath> images, OutputDirectory outputDirectory);
 
         /// <summary>
         /// Merges all images into one pdf called outputName.
         /// Create a specific type for the output ?
         /// </summary>
-        /// <param name="images"></param>
-        /// <param name="outputName"></param>
-        void Merge(IEnumerable<ImagePath> images, string outputName);
+        /// <param name="images">Collections of images to convert.</param>
+        /// <param name="outputFileName">Pdf File containing the merged images.</param>
+        void Merge(IEnumerable<ImagePath> images, string outputFileName);
     }
 }
